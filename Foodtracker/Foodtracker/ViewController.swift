c5714d7d04dd674290af423aaf72da5c76719915
Dana Gregg <danagregg@hotmail.com>
2015-12-12 15:25:50 -0800

    Initial Foodtrack App
    Basic screen with a button label and text field

---
+++ Foodtracker/Foodtracker/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Foodtracker
+//
+//  Created by Dana Gregg on 12/12/15.
+//  Copyright © 2015 Dana Gregg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Handle the text field's user input through delegate callbacks
+        nameTextField.delegate = self
+    }
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+
+}
+
